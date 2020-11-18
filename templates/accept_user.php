1a849774dfbd9a9d8dbb088ee3bda7de0705fc5a
Nick Card <nickcard2000@gmail.com>
2020-11-18 11:41:02 -0500

    fixed the accep_user page to refresh properly

--- templates/accept_user.php
+++ templates/accept_user.php
@@ -10,7 +10,7 @@
 <body>
   <h1>Acceptance</h1>
   
-  <form method="POST" action="accept_user.php">
+  <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <?php
         include '../src/register_acceptance/accept.php';
     ?>
