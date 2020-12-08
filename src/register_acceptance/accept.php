5c11a828c30e6006bf946170de99b34c60de0912
Joey Zimmerman <joeyzimmerman17@gmail.com>
2020-12-08 01:47:02 -0500

    Add employee information when accepted as employee

--- src/register_acceptance/accept.php
+++ src/register_acceptance/accept.php
@@ -20,6 +20,29 @@
 
 
         mysqli_query($link, $accept_sql);
+
+
+        $employee_sql = <<<EOL
+        SELECT u.role_id
+        FROM users u, role r
+        WHERE u.user_id = $key AND
+        u.role_id = r.role_id;
+        EOL;
+
+        $row = mysqli_fetch_array(mysqli_query($link, $employee_sql), MYSQLI_ASSOC);
+
+        //check if user is an employee
+        if ($row['role_id'] < 5) {
+
+          //if they are an employee, insert base info
+          $employee_info = <<<EOL
+          INSERT INTO employee_info (user_id, salary, group_id)
+          VALUES ($key, 0, NULL)
+          EOL;
+
+          mysqli_query($link, $employee_info);
+        }
+
       }
     } elseif (isset($_POST['deny_sub'])) {
 
